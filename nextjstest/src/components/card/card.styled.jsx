6d30620054ce91f48ef43cb5573fc10187b840e5
Jaime Sarrion <jaime.sarrion@gmail.com>
2023-10-02 17:59:42 +0200

    add color to the border on the card component

--- nextjstest/src/components/card/card.styled.jsx
+++ nextjstest/src/components/card/card.styled.jsx
@@ -4,7 +4,7 @@ import styled, { css } from "styled-components";
 export const Container = styled.div`
   position: relative;
   border-radius: 8px;
-  border: 2px solid;
+  border: 2px solid ${({ $borderColor }) => $borderColor};
   box-shadow: 4px 6px 0px 0px ${({ $borderColor }) => $borderColor};
   background-color: ${({ $background }) => $background};
 `;
